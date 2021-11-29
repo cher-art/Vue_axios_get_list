b163b58bd34aa5bfffaab854d00e264801f1ca7b
Artem Chernyshov <cher.tim22@gmail.com>
2021-11-29 17:48:15 +0200

    Create_axios_app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Api/>
+  </div>
+</template>
+
+<script>
+import Api from './services/servicesApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Api
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/services/servicesApi.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="app">
+    <div class="search-wrapper">
+       <label>
+            <input type="text" v-model="search" placeholder="Search title.."/>
+            Search hier:
+            <p>{{ this.serach }}</p>
+        </label>
+    </div>
+    <div class="wrapper">
+        <li v-for="name in this.names " :key="name">
+            {{ name }}
+        </li>
+    </div>
+</div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            search: '',
+            names: [],
+        }
+    },
+    mounted() {
+        axios
+            .get('Вставить УРЛ' + this.search)
+            .then((response) => {this.names = response.data.results})
+            .catch(error => console.log(error));
+            console.log(this.search);
+            
+    },
+}
+
+</script>
